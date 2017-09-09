644f19415a2a82a8fc7b9953d1bf3d1016d4d705
AnDong <liaoad_space@sina.com>
2017-09-09 18:26:01 +0800

    线程池

--- data-engine/src/main/java/github/eddy/bigdata/bilibili/task/CrawlerTask.java
+++ data-engine/src/main/java/github/eddy/bigdata/bilibili/task/CrawlerTask.java
@@ -1,18 +1,22 @@
 package github.eddy.bigdata.bilibili.task;
 
-import static github.eddy.bigdata.core.common.TableEnum.source;
-import static github.eddy.common.DateTools.getDateRangeYYYYMMDD;
-import static github.eddy.common.DateTools.getYYYYMM;
-
 import github.eddy.bigdata.bilibili.common.CategoryMap;
 import github.eddy.bigdata.bilibili.task.crawler.messages.SearchRequest;
 import github.eddy.bigdata.bilibili.task.crawler.messages.SearchResponse;
 import github.eddy.bigdata.core.dao.MongodbDao;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import lombok.experimental.UtilityClass;
-import lombok.extern.slf4j.Slf4j;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static github.eddy.bigdata.core.common.TableEnum.source;
+import static github.eddy.common.DateTools.getDateRangeYYYYMMDD;
+import static github.eddy.common.DateTools.getYYYYMM;
 
 /**
  * 数据抓取
@@ -21,26 +25,31 @@ import lombok.extern.slf4j.Slf4j;
 @UtilityClass
 public class CrawlerTask {
 
-  public static void search(int year, int month) {
-    String sourceTable = source.getTableName("search", getYYYYMM(year, month));
-    MongodbDao.drop(sourceTable);
-    log.info("清空元数据");
+    public static void search(int year, int month) {
+        String sourceTable = source.getTableName("search", getYYYYMM(year, month));
+        MongodbDao.drop(sourceTable);
+        log.info("清空元数据");
 
-    String[] dateRange = getDateRangeYYYYMMDD(year, month);
-    CategoryMap.getCategoryIds().forEach(cateId -> {
-      SearchRequest request = new SearchRequest(cateId, dateRange[0], dateRange[1]);
-      while (request.hasNext()) {
-        try {
-          SearchResponse response = request.next();
-          List<Map> data = response.getResult();
-          MongodbDao.insert(sourceTable, data);
-        } catch (IOException e) {
-          log.error("获取数据失败:" + request.getLongURL(), e);
+        String[] dateRange = getDateRangeYYYYMMDD(year, month);
+        Set<Integer> cateIds = CategoryMap.getCategoryIds();
+
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+        for (Integer cateId : cateIds) {
+            cachedThreadPool.execute(() -> {
+                SearchRequest request = new SearchRequest(cateId, dateRange[0], dateRange[1]);
+                while (request.hasNext()) {
+                    try {
+                        SearchResponse response = request.next();
+                        List<Map> data = response.getResult();
+                        MongodbDao.insert(sourceTable, data);
+                    } catch (IOException e) {
+                        log.error("获取数据失败:" + request.getLongURL(), e);
+                    }
+                }
+                log.info("{}的视频已处理完成", CategoryMap.getCategoryName(cateId));
+            });
         }
-      }
-      log.info("{}的视频已处理完成", CategoryMap.getCategoryName(cateId));
-    });
-    log.info("{}-{}视频数据已全部插入到{}", dateRange[0], dateRange[1], sourceTable);
-  }
+        log.info("{}-{}视频数据已全部插入到{}", dateRange[0], dateRange[1], sourceTable);
+    }
 
 }
--- data-engine/src/main/java/github/eddy/bigdata/core/configuration/MongoManager.java
+++ data-engine/src/main/java/github/eddy/bigdata/core/configuration/MongoManager.java
@@ -1,6 +1,7 @@
 package github.eddy.bigdata.core.configuration;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import lombok.experimental.UtilityClass;
@@ -10,23 +11,36 @@ import org.bson.Document;
 @UtilityClass
 public class MongoManager {
 
-  private static final MongoClient mongoClient;
-  private static final MongoDatabase mongoDB;
+    private static final MongoClient mongoClient;
+    private static final MongoDatabase mongoDB;
 
-  static {
-    mongoClient = new MongoClient("localhost", 27017);
-    mongoDB = mongoClient.getDatabase("bilibili");
-  }
+    static {
+        MongoClientOptions.Builder build = new MongoClientOptions.Builder();
+        build.connectionsPerHost(10);   //与目标数据库能够建立的最大connection数量为50
+        build.threadsAllowedToBlockForConnectionMultiplier(10); //如果当前所有的connection都在使用中，则每个connection上可以有50个线程排队等待
+            /*
+             * 一个线程访问数据库的时候，在成功获取到一个可用数据库连接之前的最长等待时间为2分钟
+             * 这里比较危险，如果超过maxWaitTime都没有获取到这个连接的话，该线程就会抛出Exception
+             * 故这里设置的maxWaitTime应该足够大，以免由于排队线程过多造成的数据库访问失败
+             */
+        build.maxWaitTime(1000 * 60 * 2);
+        build.connectTimeout(1000 * 60 * 1);    //与数据库建立连接的timeout设置为1分钟
 
-  public static MongoCollection<Document> getCollection(String collectionName) {
-    return mongoDB.getCollection(collectionName);
-  }
+        MongoClientOptions options = build.build();
 
-  //----------------------------------------------------------------------------------
-  public static Configuration mongoConfig(String input, String output) {
-    Configuration mongoConfig = new Configuration();
-    mongoConfig.set("mongo.input.uri", "mongodb://localhost/bilibili." + input);
-    mongoConfig.set("mongo.output.uri", "mongodb://localhost/bilibili." + output);
-    return mongoConfig;
-  }
+        mongoClient = new MongoClient("localhost", options);
+        mongoDB = mongoClient.getDatabase("bilibili");
+    }
+
+    public static MongoCollection<Document> getCollection(String collectionName) {
+        return mongoDB.getCollection(collectionName);
+    }
+
+    //----------------------------------------------------------------------------------
+    public static Configuration mongoConfig(String input, String output) {
+        Configuration mongoConfig = new Configuration();
+        mongoConfig.set("mongo.input.uri", "mongodb://localhost/bilibili." + input);
+        mongoConfig.set("mongo.output.uri", "mongodb://localhost/bilibili." + output);
+        return mongoConfig;
+    }
 }
