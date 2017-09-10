7d0b85efe959d6e0c0e3f8dde1a95fb26e988231
AnDong <liaoad_space@sina.com>
2017-09-10 23:42:32 +0800

    抽象提取函数

---
+++ data-engine/src/main/java/github/eddy/bigdata/bilibili/task/crawler/ApiArchive.java
@@ -0,0 +1,8 @@
+package github.eddy.bigdata.bilibili.task.crawler;
+
+/**
+ * Created by AnDong on 2017/9/10.
+ */
+public class ApiArchive {
+    //https://api.bilibili.com/archive_rank/getarchiverankbypartion?&type=jsonp&tid=19&pn=2
+}
