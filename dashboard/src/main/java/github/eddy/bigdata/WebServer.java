05b2eda0b9667687cea169e9fd9bfdcf12cae06e
阿东 <liaoad_space@sina.com>
2017-10-10 18:15:10 +0800

    绘制页面

---
+++ dashboard/src/main/java/github/eddy/bigdata/WebServer.java
@@ -0,0 +1,70 @@
+package github.eddy.bigdata;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+
+import java.io.IOException;
+
+/**
+ * Vertx 搭建的Http Web服务器
+ */
+public class WebServer {
+    private final Vertx vertx = Vertx.vertx();
+    private final MongoClient mongo = MongoClient.createShared(vertx,
+            new JsonObject().put("connection_string", "mongodb://localhost:27017").put("db_name", "bilibili"));
+
+    public static void main(String[] args) throws IOException {
+        WebServer webServer = new WebServer();
+        webServer.start();
+    }
+
+    public void start() throws IOException {
+        Router router = Router.router(vertx);
+
+        staticRoute(router);
+        mongoRoute(router);
+
+        vertx.createHttpServer().requestHandler(router::accept).listen(9999);
+    }
+
+    private void staticRoute(Router router) {
+        //reset vertx root path
+        String path = System.getProperty("user.dir");
+        System.setProperty("vertx.cwd", path);
+
+        //redirect
+        router.route("/static/*").handler(StaticHandler.create("webapp").setCachingEnabled(false));
+    }
+
+    private void restRoute(Router router) {
+
+    }
+
+    private void mongoRoute(Router router) {
+        router.get("/api/analysis/bilibili/categorydata/:date").handler(ctx -> {
+            //capturing param in url
+            String date = ctx.request().getParam("date");
+
+            //find in mongo
+            mongo.find("analysis_categorydata_" + date, new JsonObject(), lookup -> {
+                if (lookup.failed()) {
+                    ctx.fail(lookup.cause());
+                    return;
+                }
+
+                final JsonArray json = new JsonArray();
+                for (JsonObject o : lookup.result()) {
+                    json.add(o);
+                }
+
+                ctx.response().putHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+                ctx.response().end(json.encode());
+            });
+        });
+    }
+}
